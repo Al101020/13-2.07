87721b30c19392e51075728daf9c7438d30edaf7
Al101020 <alex.vl.m@yandex.ru>
2025-02-06 13:16:58 +1000

    commit-6 close

--- src/components/board/addTaskAndForm.js
+++ src/components/board/addTaskAndForm.js
@@ -28,10 +28,13 @@ function formClickAdd(e) {
 
   const divClose = document.createElement('div');
   divClose.textContent = 'x';
-  divClose.classList.add('close');
-  divClose.addEventListener('click', (event) => {
+  divClose.classList.add('divClose');
+  divClose.classList.add('displayNone');
+  function removeParent(event) {
+    divClose.removeEventListener('click', removeParent);
     event.target.parentElement.remove();
-  });
+  }
+  divClose.addEventListener('click', removeParent);
   cart.appendChild(divClose);
 
   const p = document.createElement('p');
@@ -49,13 +52,27 @@ function formClickAdd(e) {
   }
 
   cart.appendChild(p);
+  function cartMouseover() {
+    if (divClose.classList.contains('displayNone')) {
+      divClose.classList.remove('displayNone');
+    }
+  }
+
+  function cartMouseout() {
+    if (!divClose.classList.contains('displayNone')) {
+      divClose.classList.add('displayNone');
+    }
+  }
+
+  cart.addEventListener('mouseover', cartMouseover);
+  cart.addEventListener('mouseout', cartMouseout);
 
   block.appendChild(cart);
 }
 
 const buttonAdd = board.querySelectorAll('.buttonAdd');
 
-for (let i = 0; i < addLink.length; i += 1) {
+for (let i = 0; i < buttonAdd.length; i += 1) {
   buttonAdd[i].addEventListener('click', (e) => {
     formClickAdd(e);
   });
@@ -74,7 +91,7 @@ function formClickX(e) {
 
 const buttonX = board.querySelectorAll('.buttonX');
 
-for (let i = 0; i < addLink.length; i += 1) {
+for (let i = 0; i < buttonX.length; i += 1) {
   buttonX[i].addEventListener('click', (e) => {
     formClickX(e);
   });
